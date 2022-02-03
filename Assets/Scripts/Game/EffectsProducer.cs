8e94748399227a932b0bb31c98eacac4d7a7cad7
Razrob <razrobs@yandex.ru>
2022-02-03 23:03:30 +0300

    effects fix

--- Assets/Scripts/Game/EffectsProducer.cs
+++ Assets/Scripts/Game/EffectsProducer.cs
@@ -15,37 +15,36 @@ namespace Quiz
         [SerializeField] private float _incorrectEffectStrength;
         [SerializeField] private int _incorrectEffectVibrato;
 
-        private bool _effectInProgress;
+        private bool _icorrectEffectInProgress;
 
         protected override void Initialize()
         {
+            _icorrectEffectInProgress = false;
+
             commonData.UserInteractData.OnCorrectChoiceReceive += OnCorrectChoiceReceive;
             commonData.UserInteractData.OnIncorrectChoiceReceive += OnIncorrectChoiceReceive;
         }
 
         private void OnCorrectChoiceReceive(CellData cellData)
         {
-            if (_effectInProgress)
-                return;
-
-            _effectInProgress = true;
-
             CellPresenter cellPresenter = cellData.AttachedCellPresenter;
             DOTween.To(() => 0f, time => cellPresenter.Image.transform.localScale = Vector3.one * _correctBounceCurve.Evaluate(time),
-                1f, _correctBounceDuration)
-                .OnComplete(() => _effectInProgress = false);
+                1f, _correctBounceDuration);
+
+            commonData.UserInteractData.OnCorrectChoiceReceive -= OnCorrectChoiceReceive;
+            commonData.UserInteractData.OnIncorrectChoiceReceive -= OnIncorrectChoiceReceive;
         }
 
         private void OnIncorrectChoiceReceive(CellData cellData)
         {
-            if (_effectInProgress)
+            if (_icorrectEffectInProgress)
                 return;
 
-            _effectInProgress = true;
+            _icorrectEffectInProgress = true;
 
             CellPresenter cellPresenter = cellData.AttachedCellPresenter;
             cellPresenter.Image.transform.DOShakePosition(_incorrectEffectDuration, _incorrectEffectStrength, _incorrectEffectVibrato)
-                .OnComplete(() => _effectInProgress = false);
+                .OnComplete(() => _icorrectEffectInProgress = false);
         }
     }
 }
